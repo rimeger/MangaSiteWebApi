f81aa9a79df9d68c7844743cdabf1addb98d2ff7
rimeger <qfarmir@gmail.com>
2023-10-30 19:19:59 +0100

    generic repository

---
+++ Manga/Repositories/IRepositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Manga.Repositories.IRepositories
+{
+    public interface IRepository<T> where T: class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync (Guid id);
+        Task CreateAsync (T entity);
+        Task RemoveAsync (T entity);
+        Task SaveAsync();
+    }
+}
