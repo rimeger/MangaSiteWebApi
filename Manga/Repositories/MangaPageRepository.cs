218b1b99e1463542a749fe78e587207c4e0d10c3
rimeger <qfarmir@gmail.com>
2023-11-04 13:50:45 +0100

    repository implementation

---
+++ Manga/Repositories/MangaPageRepository.cs
@@ -0,0 +1,27 @@
+﻿using Manga.Data;
+using Manga.Models;
+using Manga.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manga.Repositories
+{
+    public class MangaPageRepository : Repository<MangaPage>, IMangaPageRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public MangaPageRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<MangaPage>> GetAllByChapterAsync(MangaChapter mangaChapter)
+        {
+            return await _dbContext.MangaPages.Where(mp => mp.MangaChapter == mangaChapter).ToListAsync();
+        }
+
+        public async Task UpdateAsync(MangaPage mangaPage)
+        {
+            _dbContext.MangaPages.Update(mangaPage);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Manga/Repositories/MangaTitleRepository.cs
@@ -0,0 +1,22 @@
+﻿using Manga.Data;
+using Manga.Models;
+using Manga.Repositories.IRepositories;
+
+namespace Manga.Repositories
+{
+    public class MangaTitleRepository : Repository<MangaTitle>, IMangaTitleRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public MangaTitleRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }   
+
+        public async Task UpdateAsync(MangaTitle entity)
+        {
+            _dbContext.MangaTitles.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
