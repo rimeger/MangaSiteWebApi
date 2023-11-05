79fb6f1c4d88e8c3b61175bbf11f076d481803fe
rimeger <qfarmir@gmail.com>
2023-11-05 15:48:44 +0100

    Add mappingconfig for AutoMapper

---
+++ Manga/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Manga.MediatR.MangaTitle.Commands.Create;
+using Manga.MediatR.MangaTitle.Commands.Update;
+using Manga.Models;
+using Manga.Models.Dto;
+
+namespace Manga
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<MangaTitle, MangaTitleDto>().ReverseMap();
+            CreateMap<MangaTitle, CreateTitleCommand>().ReverseMap();
+            CreateMap<MangaTitle, UpdateTitleCommand>().ReverseMap();
+        }
+    }
+}
