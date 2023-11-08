b5a69e616f021f18e8501c174a4df37debe91118
rimeger <qfarmir@gmail.com>
2023-11-08 21:19:49 +0100

    Add Controller for MangaPage and requests, handlers

---
+++ Manga/MediatR/MangaPage/Commands/Create/CreatePageCommand.cs
@@ -0,0 +1,12 @@
+﻿using Manga.Models.Dto;
+using MediatR;
+
+namespace Manga.MediatR.MangaPage.Commands.Create
+{
+    public record CreatePageCommand : IRequest<MangaPageDto>
+    {
+        public string ChapterName { get; set; }
+        public int ChapterNumber { get; set; }
+        public Guid ChapterId { get; set; }
+    }
+}
---
+++ Manga/MediatR/MangaPage/Commands/Create/CreatePageCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Manga.Models.Dto;
+using MediatR;
+
+namespace Manga.MediatR.MangaPage.Commands.Create
+{
+    public class CreatePageCommandHandler : IRequestHandler<CreatePageCommand, MangaPageDto>
+    {
+        public CreatePageCommandHandler()
+        {
+            
+        }
+        public Task<MangaPageDto> Handle(CreatePageCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
