29c2f07f27bf5eceb0fb431e6ad9e87a9b47b150
rimeger <qfarmir@gmail.com>
2023-11-09 20:47:19 +0100

    Add BaseEnity and update all models

--- Manga/Models/MangaTitle.cs
+++ Manga/Models/MangaTitle.cs
@@ -1,14 +1,11 @@
 ﻿namespace Manga.Models
 {
-    public class MangaTitle
+    public class MangaTitle : BaseEntity
     {
-        public Guid Id { get; set; }
         public string TitleName { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
         public string PosterUrl { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
         public ICollection<MangaChapter> Chapters { get; set; }
     }
 }
