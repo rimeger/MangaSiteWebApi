e16a87754db63316aec735862ab451005d34d5fa
rimeger <qfarmir@gmail.com>
2023-11-25 20:03:55 +0100

    Add handlers for login and register commands

---
+++ src/Manga.Application/Features/UserFeatures/Register/RegisterHandler.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Manga.Application.Abstractions;
+using Manga.Application.Exceptions;
+using Manga.Application.Services.Interfaces;
+using Manga.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manga.Application.Features.UserFeatures.Register
+{
+    public record RegisterCommand : IRequest<string>
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class RegisterHandler : IRequestHandler<RegisterCommand, string>
+    {
+        private readonly IUserService _userService;
+        private readonly IValidator<RegisterCommand> _validator;
+        private readonly IMapper _mapper;
+        private readonly IJwtProvider _jwtProvider;
+
+        public RegisterHandler(IUserService userService, IValidator<RegisterCommand> validator,
+            IMapper mapper, IJwtProvider jwtProvider)
+        {
+            _userService = userService;
+            _validator = validator;
+            _mapper = mapper;
+            _jwtProvider = jwtProvider;
+        }
+        public async Task<string> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            await _validator.ValidateAndThrowAsync(request);
+
+            var existingUser = await _userService.GetByUserName(request.UserName);
+            if(existingUser != null)
+            {
+                throw new InvalidCredentials($"Username is already taken");
+            }
+
+            var user = _mapper.Map<User>(request);
+            user.Id = Guid.NewGuid();
+            user.CreatedDate = DateTime.Now;
+            user.UpdatedDate = DateTime.Now;
+            user.Role = "User";
+
+            await _userService.CreateAsync(user);
+
+            string token = _jwtProvider.Generate(user);
+
+            return token;
+        }
+    }
+}
---
+++ src/Manga.Application/Features/UserFeatures/Register/RegisterValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Manga.Application.Features.UserFeatures.Register
+{
+    public class RegisterValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(p => p.UserName).NotEmpty().MinimumLength(3).MaximumLength(15);
+            RuleFor(p => p.Email).NotEmpty().EmailAddress();
+            RuleFor(p => p.Password).NotEmpty().MinimumLength(8)
+                .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+        }
+    }
+}
